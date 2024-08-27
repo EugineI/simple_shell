0f451948570386c861a194ba5ffb292afe69cb13
EugineI <eugineindimuli@gmail.com>
2024-08-27 15:46:59 -0700

    shell with exit

--- shell1.c
+++ shell1.c
@@ -5,6 +5,32 @@
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/wait.h>
+
+/**
+ * exit_shell - name
+ * @argv: argument vector
+ * @mes: line message
+ * Return: nothing
+ */
+void exit_shell(char **argv, char *mes)
+{
+	free(argv);
+	free(mes);
+	exit(0);
+}
+/**
+ * end_file - name
+ * Return: nothing
+ */
+void end_file(void)
+{
+	if (feof(stdin))
+	{
+		write(1, "\n", 1);
+		exit(0);
+	}
+	perror("./shell");
+}
 /**
  * main - entry point
  * Description: simple shell, takes no arguments
@@ -22,23 +48,21 @@ int main(void)
 		write(1, "#cisfun$ ", 9);
 		line = getline(&mes, &len, stdin);
 		if (line == -1)
-		{
-			if (feof(stdin))
-				write(1, "\n", 1);
-				break;
-			perror("./shell: ");
-			return (-1);
-		} else if (line == 1)
+			end_file();
+		else if (line == 1)
 			continue;
-		token = strtok(mes, " \n");
+		token = strtok(mes, " \t|\n");
 		argv = malloc(sizeof(char *) * 1024);
 		i = 0;
 		while (token != NULL)
 		{
+			if (strcmp(token, "exit") == 0)
+				exit_shell(argv, mes);
 			argv[i] = token;
-			token = strtok(NULL, "| \n");
+			token = strtok(NULL, "\t| \n");
 			i++;
-		} argv[i] = NULL;
+		}
+		argv[i] = NULL;
 		child_pid = fork();
 		if (child_pid == 0)
 		{
