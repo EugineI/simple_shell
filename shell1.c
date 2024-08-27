18ca232ee95a29dcc19990c596731371e71043f7
EugineI <eugineindimuli@gmail.com>
2024-08-27 11:11:47 -0700

    shell2

--- shell1.c
+++ shell1.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
+#include"shell.h"
 #include<string.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/wait.h>
-
-extern char **environ;
 /**
  * main - entry point
  * Description: simple shell, takes no arguments
@@ -13,56 +12,44 @@ extern char **environ;
  */
 int main(void)
 {
-	char *token, *mes = NULL;
-	size_t len = 0;
-	size_t line;
+	char **argv, *token, *mes = NULL;
+	size_t line, len = 0;
 	pid_t child_pid;
 	int status, i = 0;
-	char **argv;
 
-	while(1)
+	while (1)
 	{
 		write(1, "#cisfun$ ", 9);
 		line = getline(&mes, &len, stdin);
 		if (line == -1)
 		{
-			if(feof(stdin))
-			{
+			if (feof(stdin))
 				write(1, "\n", 1);
 				break;
-			}
 			perror("./shell: ");
 			return (-1);
-		}
-		else if (line == 1)
+		} else if (line == 1)
 			continue;
-		else
+		token = strtok(mes, " \n");
+		argv = malloc(sizeof(char *) * 1024);
+		i = 0;
+		while (token != NULL)
 		{
-			token = strtok(mes, "\t\n");
-			argv = malloc(sizeof(char *) * 1024);
-			i = 0;
-			while(token != NULL)
-			{
-				argv[i] = token;
-				token = strtok(NULL, "\t\n");
-				i++;
-			}
-			argv[i] = NULL;
-			child_pid = fork();
-			if (child_pid == 0)
-			{
-				if(execve(argv[0], argv, environ) == -1)
-					perror("./shell: ");
-				exit(EXIT_FAILURE);
-			}
-			else if(child_pid > 0)
-				wait(&status);
-			else
+			argv[i] = token;
+			token = strtok(NULL, "| \n");
+			i++;
+		} argv[i] = NULL;
+		child_pid = fork();
+		if (child_pid == 0)
+		{
+			if (execve(argv[0], argv, (char *const *)environ) == -1)
 				perror("./shell: ");
-			free(argv);
-		}
-
-	}
-	free(mes);
-	return(0);
+			exit(EXIT_FAILURE);
+		} else if (child_pid > 0)
+			wait(&status);
+		else
+			perror("./shell: ");
+		free(argv);
+	} free(mes);
+	return (0);
 }
