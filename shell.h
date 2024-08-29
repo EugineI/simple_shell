fb18d167014413720fb981a1abe22efbc5cc9f9e
EugineI <eugineindimuli@gmail.com>
2024-08-29 15:26:41 -0700

    test7

--- shell.h
+++ shell.h
@@ -7,7 +7,7 @@
 #include<sys/types.h>
 #include<sys/wait.h>
 #include<errno.h>
-#define BUFFER_SIZE 1024;
+#define BUFFER_SIZE 1024
 extern char **environ;
 void exit_shell(char **argv, char *mes);
 void end_file(char *mes);
