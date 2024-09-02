5c10c4e1f16e3a69f0815940a9672237a29f352e
EugineI <eugineindimuli@gmail.com>
2024-09-02 14:09:40 -0700

    -cd-

--- cd.c
+++ cd.c
@@ -7,22 +7,20 @@
 int _cd(char **args)
 {
 	char *dir, cwd[1024];
-	static char prev_dir[1024] = "";
+	char *prev_dir = getenv("OLDPWD");
 
 	if (args[1] == NULL)
 	{
 		dir = getenv("HOME");
 		if (dir == NULL)
 		{
-			fprintf(stderr, "cd: No home directory\n");
 			return (1);
 		}
 	}
 	else if (strcmp(args[1], "_") == 0)
 	{
-		if (strlen(prev_dir) == 0)
+		if (prev_dir == NULL)
 		{
-			fprintf(stderr, "cd: OLDPWD not set\n");
 			return (1);
 		}
 		dir = prev_dir;
@@ -31,21 +29,19 @@ int _cd(char **args)
 	{
 		dir = args[1];
 	}
-	if (getcwd(prev_dir, sizeof(prev_dir)) == NULL)
+	if (getcwd(cwd, sizeof(cwd)) == NULL)
 	{
 		perror("getcwd");
 		return (1);
 	}
 	if (chdir(dir) != 0)
 	{
-		perror("cd");
 		return(1);
 	}
 	if (getcwd(cwd, sizeof(cwd)) != NULL)
 		setenv("PWD", cwd, 1);
 	else
 	{
-		perror("getcwd");
 		return (1);
 	}
 	return 0;
